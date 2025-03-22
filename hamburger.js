441b2ffb663bd86a206f2da43e4692a9247d5004
Ralfino26 <ralf.hofman@student.kdg.be>
2025-03-22 13:57:42 +0100

    Fixed Mobile friendly

---
+++ hamburger.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.navbar ul');
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
+});
