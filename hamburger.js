ed6b2390d3ac068854a99a478fd1fac050385a29
Ralfino26 <ralf.hofman@student.kdg.be>
2025-03-23 22:27:14 +0100

    COMPLETE

--- hamburger.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const menu = document.querySelector('.navbar ul');
-    hamburger.addEventListener('click', () => {
-        menu.classList.toggle('show');
-    });
-});
